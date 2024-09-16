2ca5b4c4ec5c3fc05760a6aec33fc2fc0061512e
shashankd123 <71559885+shashankd123@users.noreply.github.com>
2024-09-16 21:05:10 +0530

    Added play game function

--- script.js
+++ script.js
@@ -10,34 +10,49 @@ function getHumanChoice(){
     return UserInput
 }
 
-let humanChoice = (getHumanChoice()).toLowerCase();
-// console.log(humanChoice)
-let computerChoice = getComputerChoice();
-
-let humanScore = 0;
-let computerScore = 0;
-
 function playRound(humanChoice, computerChoice){
     if (humanChoice == computerChoice){
         return "Draw"
     }else if (humanChoice == "rock" && computerChoice == "scissors"){
-        humanScore +=1
-        return ["You win", `Your score = ${humanScore}`, `Computer score = ${computerScore}`]
+        // humanScore +=1
+        return "You win"
     }else if (humanChoice == "paper" && computerChoice == "rock"){
-        humanScore +=1
-        return ["You win", `Your score = ${humanScore}`, `Computer score = ${computerScore}`]
+        // humanScore +=1
+        return "You win"
     }else if (humanChoice == "scissors" && computerChoice == "paper"){
-        humanScore +=1
-        return ["You win", `Your score = ${humanScore}`, `Computer score = ${computerScore}`]
+        // humanScore +=1
+        return "You win"
     }else if ((humanChoice != "rock") && (humanChoice != "paper") && (humanChoice != "scissors")){
-        return ["Wrong input", `Your score = ${humanScore}`, `Computer score = ${computerScore}`]
+        return "Wrong input"
     }else{
-        computerScore += 1
-        return ["You lose", `Your score = ${humanScore}`, `Computer score = ${computerScore}`]
+        // computerScore += 1
+        return "You lose"
     }
 }
 
-console.log(playRound(humanChoice, computerChoice))
+function playGame(){
+    let result = []
+    let i = 0;
+    let humanScore = 0;
+    let computerScore = 0; 
+    while (i < 5){
+        i = i+1;
+        let humanChoice = (getHumanChoice()).toLowerCase();
+        let computerChoice = getComputerChoice();
+        let res = playRound(humanChoice, computerChoice);
+        result.push(res)
+        if(res == "You win"){
+            humanScore += 1;
+        }else if(res == "You lose"){
+            computerScore += 1;
+        }
+    }
+    return [result, `Your score = ${humanScore}`, `Computer score = ${computerScore}`]
+}
+
+console.log(playGame())
+
+// console.log(playRound(humanChoice, computerChoice))
 // console.log(humanChoice)
 // console.log(computerChoice)
 
